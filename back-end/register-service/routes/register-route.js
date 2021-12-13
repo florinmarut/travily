657605ba9d53d6abac31da172ede86e4913aa390
AlexandruMorar00 <alexandru.morar00@gmail.com>
2021-12-13 20:14:15 +0200

    forms

--- back-end/register-service/routes/register-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// import controllers
-const register = require('../controllers/register-controller')
-
-router.post('/', register);
-
-module.exports = router;
